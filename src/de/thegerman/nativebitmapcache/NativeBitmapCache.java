757243efed43d51ed1bd0699a7bd9e63add0601e
Richard Meissner <rmeissner@team.mobile.de>
2014-07-30 15:22:30 +0200

    initial commit

---
+++ src/de/thegerman/nativebitmapcache/NativeBitmapCache.java
@@ -0,0 +1,33 @@
+package de.thegerman.nativebitmapcache;
+
+import android.graphics.Bitmap;
+
+import com.squareup.picasso.Cache;
+
+public class NativeBitmapCache implements Cache {
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Bitmap get(String arg0) {
+        return null;
+    }
+
+    @Override
+    public int maxSize() {
+        return 0;
+    }
+
+    @Override
+    public void set(String arg0, Bitmap arg1) {
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+}
