5865cb7f85068d4890046cc3f2f25a1efd8ef58c
Richard Meissner <rmeissner@team.mobile.de>
2014-07-30 16:20:20 +0200

    add native calls

--- src/de/thegerman/nativebitmapcache/NativeBitmapCache.java
+++ src/de/thegerman/nativebitmapcache/NativeBitmapCache.java
@@ -1,6 +1,9 @@
 package de.thegerman.nativebitmapcache;
 
+import java.nio.ByteBuffer;
+
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import com.squareup.picasso.Cache;
 
@@ -8,12 +11,15 @@ public class NativeBitmapCache implements Cache {
 
     @Override
     public void clear() {
-
+        nativeClear();
     }
 
     @Override
-    public Bitmap get(String arg0) {
-        return null;
+    public Bitmap get(String key) {
+        NativeBitmapCacheEntry entry = nativeGetImageData(key);
+        if (entry == null) return null;
+        Bitmap newImage = BitmapFactory.decodeByteArray(entry.imageData, 0, entry.imageData.length);
+        return newImage;
     }
 
     @Override
@@ -22,12 +28,24 @@ public class NativeBitmapCache implements Cache {
     }
 
     @Override
-    public void set(String arg0, Bitmap arg1) {
+    public void set(String key, Bitmap image) {
+        int imageWidth = image.getWidth();
+        int imageHeight = image.getHeight();
+        final int imageBytes = imageWidth * imageHeight * 4;
+        ByteBuffer imageBuffer = ByteBuffer.allocateDirect(imageBytes);
+        image.copyPixelsToBuffer(imageBuffer);
+        nativeStoreImageData(key, imageBuffer, imageWidth, imageHeight);
     }
 
     @Override
     public int size() {
         return 0;
     }
+
+    private native NativeBitmapCacheEntry nativeGetImageData(String key);
+
+    private native void nativeStoreImageData(String key, ByteBuffer imageBuffer, int imageWidth, int imageHeight);
+
+    private native void nativeClear();
 
 }
---
+++ src/de/thegerman/nativebitmapcache/NativeBitmapCacheEntry.java
@@ -0,0 +1,9 @@
+package de.thegerman.nativebitmapcache;
+
+
+public class NativeBitmapCacheEntry {
+    public byte[] imageData;
+    public int imageWidth;
+    public int imageHeight;
+
+}
